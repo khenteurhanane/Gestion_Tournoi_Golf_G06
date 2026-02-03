67bcd51cf9de8fe8d8d82d1a9d18e1677e18ae1a
Rayan terki <2733003@collegelacite.ca>
2026-02-03 01:56:11 -0500

    GOLF-30 GOLF-31: Validation et contrôleur participant

--- Models/Participant.cs
+++ Models/Participant.cs
@@ -1,12 +1,27 @@
-﻿namespace croupe_06_TournoiGolf.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace croupe_06_TournoiGolf.Models
 {
     public class Participant
     {
         public int Id { get; set; }
-        public string Prenom { get; set; }  
+
+        [Required(ErrorMessage = "Le prénom est requis")]
+        [StringLength(50, ErrorMessage = "Maximum 50 caractères")]
+        public string Prenom { get; set; }
+
+        [Required(ErrorMessage = "Le nom est requis")]
+        [StringLength(50, ErrorMessage = "Maximum 50 caractères")]
         public string Nom { get; set; }
+
+        [Required(ErrorMessage = "Le courriel est requis")]
+        [EmailAddress(ErrorMessage = "Format de courriel invalide")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Le téléphone est requis")]
+        [Phone(ErrorMessage = "Format de téléphone invalide")]
         public string Telephone { get; set; }
+
         public string MotDePasseHash { get; set; }
     }
 }
