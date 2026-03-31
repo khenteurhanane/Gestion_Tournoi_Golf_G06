55994a1fe34ac903a7720a9421e71802f8c144b9
khenteurhanane <2741839@collegelacite.ca>
2026-03-31 11:19:00 -0400

    [US-11-T02] Afficher les types de commandites disponibles

---
+++ Controllers/SponsorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace croupe_06_TournoiGolf.Controllers
+{
+    public class SponsorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
